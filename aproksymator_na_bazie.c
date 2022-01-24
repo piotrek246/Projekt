cd87938741d7d23d6ea832dec259d5dccbf044bf
piotrek246 <96173138+piotrek246@users.noreply.github.com>
2022-01-24 02:56:41 +0100

    Add files via upload

---
+++ aproksymator_na_bazie.c
@@ -0,0 +1,186 @@
+#include "makespl.h"
+#include "piv_ge_solver.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+/* UWAGA: liczbę używanych f. bazowych można ustawić przez wartość
+          zmiennej środowiskowej APPROX_BASE_SIZE
+*/
+
+/*
+ * Funkcje bazowe: n - liczba funkcji a,b - granice przedzialu aproksymacji i
+ * - numer funkcji x - wspolrzedna dla ktorej obliczana jest wartosc funkcji
+ */
+double
+fi(int n, double x)
+{
+double l0(double x){
+    return 1;
+}
+ 
+double l1(double x){
+    return -x+1;
+}
+
+double ln(int n, double x){
+    if(n==0){
+        return l0(x);
+    }
+    else if(n==1){
+        return l1(x);
+    }
+    else{
+        return ((2*(n-1)+1-x)*ln(x,n-1)-(n-1)*ln(x,n-2))/n;
+    }
+}
+}
+
+/* Pierwsza pochodna fi */
+double
+dfi(int n, double x)
+{
+	return (n*(fi(n, x))-n*(fi(n-1, x)))/x; 
+}
+
+/* Druga pochodna fi */
+double
+d2fi( int n, double x)
+{
+	return (n*(dfi(n, x))-n*(dfi(n-1, x)))/x; 
+
+}
+
+/* Trzecia pochodna fi */
+double
+d3fi( int n, double x)
+{
+	return (n*(d2fi(n, x))-n*(d2fi(n-1, x)))/x; 
+
+}
+
+/* Pomocnicza f. do rysowania bazy */
+double
+xfi(double a, double b, int n, int i, FILE *out)
+{
+	double		h = (b - a) / (n - 1);
+	double		h3 = h * h * h;
+	int		hi         [5] = {i - 2, i - 1, i, i + 1, i + 2};
+	double		hx      [5];
+	int		j;
+
+	for (j = 0; j < 5; j++)
+		hx[j] = a + h * hi[j];
+
+	fprintf( out, "# nb=%d, i=%d: hi=[", n, i );
+	for( j= 0; j < 5; j++ )
+		fprintf( out, " %d", hi[j] );
+	fprintf( out, "] hx=[" );
+	for( j= 0; j < 5; j++ )
+		fprintf( out, " %g", hx[j] );
+	fprintf( out, "]\n" );
+}
+
+void
+make_spl(points_t * pts, spline_t * spl)
+{
+
+	matrix_t       *eqs= NULL;
+	double         *x = pts->x;
+	double         *y = pts->y;
+	double		a = x[0];
+	double		b = x[pts->n - 1];
+	int		i, j, k;
+	int		nb = pts->n - 3 > 10 ? 10 : pts->n - 3;
+  char *nbEnv= getenv( "APPROX_BASE_SIZE" );
+
+	if( nbEnv != NULL && atoi( nbEnv ) > 0 )
+		nb = atoi( nbEnv );
+
+	eqs = make_matrix(nb, nb + 1);
+/*
+#ifdef DEBUG
+#define TESTBASE 500
+	{
+		FILE           *tst = fopen("debug_base_plot.txt", "w");
+		double		dx = (b - a) / (TESTBASE - 1);
+		for( j= 0; j < nb; j++ )
+			xfi( a, b, nb, j, tst );
+		for (i = 0; i < TESTBASE; i++) {
+			fprintf(tst, "%g", a + i * dx);
+			for (j = 0; j < nb; j++) {
+				fprintf(tst, " %g", fi  (nb,  a + i * dx));
+				fprintf(tst, " %g", dfi (nb, a + i * dx));
+				fprintf(tst, " %g", d2fi(a, b, nb, j, a + i * dx));
+				fprintf(tst, " %g", d3fi(a, b, nb, j, a + i * dx));
+			}
+			fprintf(tst, "\n");
+		}
+		fclose(tst);
+	}
+#endif
+*/
+	for (j = 0; j < nb; j++) {
+		for (i = 0; i < nb; i++)
+			for (k = 0; k < pts->n; k++)
+				add_to_entry_matrix(eqs, j, i, fi(nb,  x[k]) * fi(j, x[k]));
+
+		for (k = 0; k < pts->n; k++)
+			add_to_entry_matrix(eqs, j, nb, y[k] * fi(nb, x[k]));
+	}
+
+#ifdef DEBUG
+	write_matrix(eqs, stdout);
+#endif
+
+	if (piv_ge_solver(eqs)) {
+		spl->n = 0;
+		return;
+	}
+#ifdef DEBUG
+	write_matrix(eqs, stdout);
+#endif
+
+	if (alloc_spl(spl, nb) == 0) {
+		for (i = 0; i < spl->n; i++) {
+			double xx = spl->x[i] = a + i*(b-a)/(spl->n-1);
+			xx+= 10.0*DBL_EPSILON;  // zabezpieczenie przed ulokowaniem punktu w poprzednim przedziale
+			spl->f[i] = 0;
+			spl->f1[i] = 0;
+			spl->f2[i] = 0;
+			spl->f3[i] = 0;
+			for (k = 0; k < nb; k++) {
+				double		ck = get_entry_matrix(eqs, k, nb);
+				spl->f[i]  += ck * fi  (nb,  xx);
+				spl->f1[i] += ck * dfi (nb,  xx);
+				spl->f2[i] += ck * d2fi(nb,  xx);
+				spl->f3[i] += ck * d3fi(nb,  xx);
+				
+			}
+		}
+	}
+/*
+#ifdef DEBUG
+	{
+		FILE           *tst = fopen("debug_spline_plot.txt", "w");
+		double		dx = (b - a) / (TESTBASE - 1);
+		for (i = 0; i < TESTBASE; i++) {
+			double yi= 0;
+			double dyi= 0;
+			double d2yi= 0;
+			double d3yi= 0;
+			double xi= a + i * dx;
+			for( k= 0; k < nb; k++ ) {
+							yi += get_entry_matrix(eqs, k, nb) * fi(nb, xi);
+							dyi += get_entry_matrix(eqs, k, nb) * dfi(a, b, nb, k, xi);
+							d2yi += get_entry_matrix(eqs, k, nb) * d2fi(a, b, nb, k, xi);
+							d3yi += get_entry_matrix(eqs, k, nb) * d3fi(a, b, nb, k, xi);
+			}
+			fprintf(tst, "%g %g %g %g %g\n", xi, yi, dyi, d2yi, d3yi );
+		}
+		fclose(tst);
+	}
+#endif
+*/
+}
